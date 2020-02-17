6a843cd15be85b35b766d34ef3bd658ee804c9e5
findot <findot@protonmail.com>
2020-02-17 20:55:43 +0100

    CI/CD - frontend

---
+++ codecov.sh
@@ -0,0 +1,11 @@
+--2020-02-17 20:54:38--  https://codecov.io/bash
+Resolving codecov.io (codecov.io)... 35.199.43.247
+Connecting to codecov.io (codecov.io)|35.199.43.247|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 49480 (48K) [text/plain]
+Saving to: ‘bash’
+
+     0K .......... .......... .......... .......... ........  100%  225K=0.2s
+
+2020-02-17 20:54:38 (225 KB/s) - ‘bash’ saved [49480/49480]
+
