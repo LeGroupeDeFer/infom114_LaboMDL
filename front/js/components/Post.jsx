8cb01c1e8a7b84d9bcf283243da49fdf8b67c453
Aurélien <aurelien_lef@hotmail.fr>
2020-02-29 19:48:38 +0100

    Improved posts page

--- front/js/components/Post.jsx
+++ front/js/components/Post.jsx
@@ -1,23 +1,76 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import { loremIpsum } from '../utils/dev';
 import { preview } from '../utils';
+import Badge from 'react-bootstrap/Badge'
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Moment from 'react-moment';
 
-const Post = ({ title, text, previewLength, ...otherProps }) => (
-  <Card {...otherProps}>
-    <Card.Body >
-      <Card.Title>{title}</Card.Title>
-      <Card.Text>{preview(text, previewLength)}</Card.Text>
-      <Button variant='primary'>Read</Button>
-    </Card.Body>
-  </Card>
-);
+const Post = ({ title, text, username, vote, type, previewLength, createdOn, ...otherProps }) => {
+
+  function getDisplayedType(type) {
+    switch (type) {
+      case 'info':
+        return 'Information';
+      case 'poll':
+        return 'Poll';
+      case 'idea':
+        return 'Idea submission';
+      case 'decisional':
+        return 'Decisional';
+    }
+  }
+
+
+  return (
+    <div>
+
+      <Card {...otherProps} className='post'>
+        <Card.Header>
+          <div style={{ fontSize: '19px' }}>
+            <Badge className={'post-' + type}>{getDisplayedType(type)} </Badge>
+            <a href='#'> {username}</a>
+            <span className="text-muted" style={{ fontSize: '14px' }}> - <Moment fromNow>{createdOn}</Moment></span>
+          </div>
+        </Card.Header>
+        <Card.Body >
+          <Row>
+            <Col xs='auto'>
+              <Button variant="light" className='thumbsUp'> <Icon icon={faArrowUp} /></Button>
+              <div className='text-center'> {vote}</div>
+              <Button variant="light" className='thumbsDown'> <Icon icon={faArrowDown}></Icon></Button>
+
+            </Col>
+
+            <Col>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>{preview(text, previewLength)}</Card.Text>
+            </Col>
+
+          </Row>
+
+        </Card.Body>
+      </Card>
+
+      <br />
+    </div>
+
+
+  );
+}
 
 Post.defaultProps = {
   title: 'A post',
   text: loremIpsum,
-  previewLength: 200
+  username: 'John Coffey',
+  previewLength: 200,
+  vote: 25,
+  type: 'info',
+  createdOn: '2020-02-29T12:59-0500'
 };
 
 export default Post;
--- front/js/pages/Stream.jsx
+++ front/js/pages/Stream.jsx
@@ -1,16 +1,27 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import usePromise from 'react-promise-suspense';
 import Post from '../components/Post';
 import { loremIpsum, fakeLatency } from '../utils/dev';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faSortAmountDownAlt } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 
 /* Delayed fetching of user posts */
 // fetchPosts :: int => Promise<Array<Object>>
 const fetchPosts = time => new Promise((resolve, _) => setTimeout(
-  () => resolve(Array(20).fill({ id: 0, title: 'A post', text: loremIpsum })),
+  () => resolve(Array(3).fill({ id: 0, title: 'A post', type: 'info', text: loremIpsum, username: 'John Coffey', vote: 18, createdOn: '2020-02-19T12:59-0500' })
+    .concat(Array(2).fill({ id: 0, title: '', type: 'poll', text: loremIpsum, username: 'John Cena', vote: 5, createdOn: '2020-02-29T12:59-0500' }))
+  ),
   time
 ));
 
@@ -31,15 +42,108 @@ const PostList = props => {
 
 // Stream :: None => Component
 const Stream = () => (
+
+
   <Container>
-    <Row>
-      <Col><h1>Stream</h1></Col>
+    <br />
+    <Row className='justify-content-md-center'>
+      <FilterBar />
     </Row>
+
+    <br />
+    <Row className='justify-content-end'>
+      <SortDropdown />
+    </Row>
+
+    <br />
     <Suspense fallback={<h3>Loading posts...</h3>}>
       <PostList />
     </Suspense>
   </Container>
 );
+
+
+// SortDropdown :: None => Component
+const SortDropdown = () => {
+
+  const [criteria, setCriteria] = useState('new');
+  const [title, setTitle] = useState('Sort by');
+  
+  return (
+    <DropdownButton
+      title={
+        <span><Icon icon={faSortAmountDownAlt}></Icon> {title}</span>
+      }
+      variant="secondary"
+    >
+      <Dropdown.Item
+        as='button'
+        onClick={() => { setCriteria('top'); setTitle('Sort by - Top') }}
+      >
+        Top
+      </Dropdown.Item>
+      <Dropdown.Item
+        as='button'
+        onClick={() => { setCriteria('new'); setTitle('Sort by - New') }}
+      >
+        New
+      </Dropdown.Item>
+      <Dropdown.Item
+        as='button'
+        onClick={() => { setCriteria('old'); setTitle('Sort by - Old') }}
+      >
+        Old
+      </Dropdown.Item>
+    </DropdownButton>
+  );
+}
+
+// FilterBar :: None => Component 
+const FilterBar = () => {
+
+  const [filter, setFilter] = useState('all');
+
+  return (
+    <ButtonGroup id='filter-bar'>
+      <Button
+        variant='secondary'
+        className={filter == 'all' ? 'active' : ''}
+        onClick={() => setFilter('all')}
+      >
+        All
+      </Button>
+      <Button
+        variant='secondary'
+        className={filter == 'decisional' ? 'active' : ''}
+        onClick={() => setFilter('decisional')}
+      >
+        Decisional
+      </Button>
+      <Button
+        variant='secondary'
+        className={filter == 'poll' ? 'active' : ''}
+        onClick={() => setFilter('poll')}
+      >
+        Poll
+      </Button>
+      <Button
+        variant='secondary'
+        className={filter == 'info' ? 'active' : ''}
+        onClick={() => setFilter('info')}
+      >
+        Information
+      </Button>
+      <Button
+        variant='secondary'
+        className={filter == 'idea' ? 'active' : ''}
+        onClick={() => setFilter('idea')}
+      >
+        Idea submission
+      </Button>
+    </ButtonGroup>
+  );
+
+}
 
 Stream.defaultProps = {};
 
