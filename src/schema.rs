697ce80d0e6d00e9cb6c1da170f7b49eafe93ada
dtngan <thuyngandao@gmail.com>
2020-03-17 12:09:22 +0100

    Update rocket version to 0.4.3 to eliminate the error

--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,30 @@
 table! {
+    posts (id) {
+        id -> Unsigned<Integer>,
+        title -> Nullable<Varchar>,
+        content -> Mediumtext,
+        authorid -> Unsigned<Integer>,
+        created_at -> Nullable<Datetime>,
+        modified_at -> Nullable<Datetime>,
+        reply_to -> Nullable<Unsigned<Integer>>,
+    }
+}
+
+table! {
+    post_tag_map (post_id, tag_id) {
+        post_id -> Unsigned<Integer>,
+        tag_id -> Unsigned<Integer>,
+    }
+}
+
+table! {
+    tags (id) {
+        id -> Unsigned<Integer>,
+        description -> Varchar,
+    }
+}
+
+table! {
     users (id) {
         id -> Unsigned<Integer>,
         email -> Varchar,
@@ -13,3 +39,26 @@ table! {
         phone -> Nullable<Varchar>,
     }
 }
+
+table! {
+    votes (post_id, vote_authorid) {
+        post_id -> Unsigned<Integer>,
+        vote_authorid -> Unsigned<Integer>,
+        voted_at -> Nullable<Datetime>,
+        is_vote_up -> Bool,
+    }
+}
+
+joinable!(post_tag_map -> posts (post_id));
+joinable!(post_tag_map -> tags (tag_id));
+joinable!(posts -> users (authorid));
+joinable!(votes -> posts (post_id));
+joinable!(votes -> users (vote_authorid));
+
+allow_tables_to_appear_in_same_query!(
+    posts,
+    post_tag_map,
+    tags,
+    users,
+    votes,
+);
