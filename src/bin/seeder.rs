46751b773fb65b0f64d9f5536b5c25d0b61c14bd
Guillaume Latour <g0latour@gmail.com>
2020-05-01 20:22:37 +0200

    fix seeder bug

--- src/bin/seeder.rs
+++ src/bin/seeder.rs
@@ -9,6 +9,7 @@ use unanimitylibrary::lib::seeds;
 /// Seed database
 fn main() {
     let conn = database::connection(&database::url());
+    seeds::clean_all_table(&conn);
     seeds::roles::seed_roles_and_capabilities(&conn);
     seeds::tags::seed_tags(&conn);
     seeds::posts::seed_test_posts(&conn);
--- tests/init.rs
+++ tests/init.rs
@@ -25,26 +25,7 @@ pub fn clean() {
     // get connection
     let conn = database_connection();
 
-    // truncate all tables
-    diesel::delete(roles_capabilities_table)
-        .execute(&conn)
-        .unwrap();
-    diesel::delete(capabilities_table).execute(&conn).unwrap();
-    diesel::delete(users_roles_table).execute(&conn).unwrap();
-    diesel::delete(posts_tags_table).execute(&conn).unwrap();
-    diesel::delete(votes_comments_table).execute(&conn).unwrap();
-    diesel::delete(votes_posts_table).execute(&conn).unwrap();
-    diesel::delete(tags_subscription_table)
-        .execute(&conn)
-        .unwrap();
-    diesel::delete(roles_table).execute(&conn).unwrap();
-    diesel::delete(tags_table).execute(&conn).unwrap();
-    diesel::delete(capabilities_table).execute(&conn).unwrap();
-    diesel::delete(posts_reports_table).execute(&conn).unwrap();
-    diesel::delete(posts_table).execute(&conn).unwrap();
-    diesel::delete(users_table).execute(&conn).unwrap();
-    diesel::delete(tokens_table).execute(&conn).unwrap();
-    diesel::delete(addresses_table).execute(&conn).unwrap();
+    seeds::clean_all_table(&conn);
 
     // assert empty database
     assert_eq!(
