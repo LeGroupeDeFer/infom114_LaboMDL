03371585252e5d3aa22c3ba83cefd53c09ece702
dtngan <thuyngandao@gmail.com>
2020-05-07 01:30:20 +0200

    #18 Add tests

--- tests/comments/crud.rs
+++ tests/comments/crud.rs
@@ -19,6 +19,8 @@ fn create_comment_from_post() {
 
     let comment_entity = CommentEntity::by_id(&conn, &comment.id).unwrap().unwrap();
     assert_eq!(comment_entity.id, comment.id);
+    assert_eq!(comment_entity.post_id, post.id);
+    assert_eq!(comment_entity.parent_id, None);
     assert_eq!(comment_entity.content, comment.content);
     assert_eq!(comment_entity.author_id, comment.author.id);
     assert_eq!(comment_entity.author_id, init::get_admin().id);
@@ -32,22 +34,154 @@ fn create_comment_from_post_duplicate_details() {
 
     let post = init::get_post_entity(false, false, false);
     let token = login_admin();
-    let comment1 = send_comment_from_post(&client, token.clone(), &post.id, "There are 2 comments like this!!");
+    let comment_content = "There are 2 comments like this!!";
+    
+    let comment1 = send_comment_from_post(&client, token.clone(), &post.id, comment_content);
+    assert!(CommentEntity::by_id(&conn, &comment1.id).unwrap().is_some());
+    
     thread::sleep(time::Duration::from_millis(10000));
-    let comment2 = send_comment_from_post(&client, token.clone(), &post.id, "There are 2 comments like this!!");
-    println!("Created times: \n{}\n{}", comment1.created_at, comment2.created_at);
-    assert!(comment1.id != comment2.id);
+
+    let comment2 = send_comment_from_post(&client, token.clone(), &post.id, comment_content);
+    assert!(CommentEntity::by_id(&conn, &comment2.id).unwrap().is_some());
+
+    assert_ne!(comment1.id, comment2.id);
     assert!(comment1.created_at < comment2.created_at);
 }
 
-// create a comment to a comment from a post
-// create a comment from a post (unauthenticated)
-// create a comment to a comment from a post (unauthenticated)
+#[test]
+fn create_comment_from_post_unauthenticated() {
+    let client = init::clean_client();
+    init::seed();
+    let conn = init::database_connection();
+
+    let post = init::get_post_entity(false, false, false);
+    let route = format!("/api/v1/post/{}/comment", post.id);
+
+    let comment_content = "This is a test comment.";
+    let json_data = format!("{{ \"content\": \"{}\" }}", comment_content);
+
+    let req = client
+        .post(route)
+        .header(ContentType::JSON)
+        .body(json_data);
+
+    let response = req.dispatch();
+
+    assert_eq!(response.status(), Status::Unauthorized);
+    assert_eq!(
+        CommentEntity::by_post_id(&conn, &post.id, false).unwrap().len(), 
+        0
+    );
+}
+
 // create a comment with a malformed json
-// create a comment from an unexisting post
+#[test]
+fn create_comment_from_post_bad_json() {
+    let client = init::clean_client();
+    init::seed();
+
+    let conn = init::database_connection();
+    let auth_header = init::login_admin();
+    let post = init::get_post_entity(false, false, false);
+    let route = format!("/api/v1/post/{}/comment", post.id);
+
+    let comment_content = "There is no quotation marks surrouding me.";
+    let bad_json = format!("{{ \"content\": {} }}", comment_content);
+
+    let req = client
+        .post(route)
+        .header(ContentType::JSON)
+        .header(auth_header)
+        .body(bad_json);
+
+    let response = req.dispatch();
+    assert_eq!(response.status(), Status::BadRequest);
+    assert_eq!(
+        CommentEntity::by_post_id(&conn, &post.id, false).unwrap().len(), 
+        0
+    );
+}
+
+#[test]
+fn create_comment_from_unexisting_post() {
+    let client = init::clean_client();
+    init::seed();
+
+    let conn = init::database_connection();
+    let mut unexisting_id = 12;
+    while PostEntity::by_id(&conn, &unexisting_id).unwrap().is_some() {
+        unexisting_id += 1;
+    }
+
+    let comment_content = "I should not be sucessfully submitted!.";
+    let response_status = send_comment_from_unavailable_post(
+        &client,
+        init::login_admin(),
+        &unexisting_id,
+        comment_content
+    );
+    assert_eq!(response_status, Status::BadRequest); 
+}
+
+#[test]
+fn create_comment_from_locked_post() {
+    let client = init::clean_client();
+    init::seed();
+    
+    let post = init::get_post_entity(true, false, false);
+    let route = format!("/api/v1/post/{}/comment", post.id);
+    let comment_content = "I am a comment, I cannot be submitted to a hidden post.";
+
+    let response_status = send_comment_from_unavailable_post(
+        &client,
+        init::login_admin(),
+        &post.id,
+        comment_content
+    );
+    assert_eq!(response_status, Status::BadRequest); 
+}
+
 // create a comment from an hidden post
+#[test]
+fn create_comment_from_hidden_post() {
+    let client = init::clean_client();
+    init::seed();
+    
+
+    let post = init::get_post_entity(false, true, false);
+    let route = format!("/api/v1/post/{}/comment", post.id);
+    let comment_content = "I am a comment, I cannot be submitted to a hidden post.";
+
+    let response_status = send_comment_from_unavailable_post(
+        &client,
+        init::login_admin(),
+        &post.id,
+        comment_content
+    );
+    assert_eq!(response_status, Status::BadRequest); 
+}
+
 // create a comment from a soft-deleted post
-// create a comment from a locked post
+#[test]
+fn create_comment_from_soft_deleted_post() {
+    let client = init::clean_client();
+    init::seed();
+    
+    let post = init::get_post_entity(false, false, true);
+    let route = format!("/api/v1/post/{}/comment", post.id);
+    let comment_content = "I am a comment, I cannot be submitted to a soft-deleted post.";
+
+    let response_status = send_comment_from_unavailable_post(
+        &client,
+        init::login_admin(),
+        &post.id,
+        comment_content
+    );
+    assert_eq!(response_status, Status::BadRequest); 
+}
+
+// create a comment to a comment from a post
+// create a comment to a comment from a post (unauthenticated)
 // create a comment to an unexisting comment from a post
 
 // get all comments from a post
--- tests/comments/helper.rs
+++ tests/comments/helper.rs
@@ -1,6 +1,25 @@
 use rocket::http::{ContentType, Status};
 use unanimitylibrary::database::models::prelude::Comment;
 
+
+pub fn send_comment_from_unavailable_post(
+    client: &rocket::local::Client,
+    auth_token: rocket::http::Header<'static>,
+    post_id: &u32,
+    comment: &str,
+) -> Status {
+    let route = format!("/api/v1/post/{}/comment", post_id);
+    let data_json = format!("{{ \"content\": \"{}\" }}", comment);
+    let req = client
+        .post(route)
+        .header(ContentType::JSON)
+        .header(auth_token)
+        .body(data_json);
+    
+    // I prefer returning a Response object but I don't know how
+    req.dispatch().status()
+}
+
 pub fn send_comment_from_post(
     client: &rocket::local::Client,
     auth_token: rocket::http::Header<'static>,
