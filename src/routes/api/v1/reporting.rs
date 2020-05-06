1ec7bbb888837dea31c34b45fcd1183767557be7
tsepton <tsepton@gmail.com>
2020-05-06 14:15:31 +0200

    Add end point for reporting routes

---
+++ src/routes/api/v1/reporting.rs
@@ -0,0 +1,46 @@
+//use crate::database::models::prelude::*;
+use crate::database::DBConnection;
+//use crate::guards::auth::Auth;
+
+use crate::http::responders::{ok, api::ApiResponse};
+use rocket::http::Status;
+
+//use crate::lib::EntityError;
+//use rocket_contrib::json::Json;
+
+pub fn collect() -> Vec<rocket::Route> {
+    routes!(get_users_report, get_tags_report, get_posts_report)
+}
+
+#[get("/api/v1/report/users")]
+pub fn get_users_report(conn: DBConnection) -> ApiResponse {
+    //let tags = TagEntity::all(&*conn).unwrap();
+    ApiResponse::new(
+        Status::Ok,
+        json!({
+            "todo" : "TODO"
+        }),
+    )
+}
+
+#[get("/api/v1/report/tags")]
+pub fn get_tags_report(conn: DBConnection) -> ApiResponse {
+    //let tags = TagEntity::all(&*conn).unwrap();
+    ApiResponse::new(
+        Status::Ok,
+        json!({
+            "todo" : "TODO"
+        }),
+    )
+}
+
+#[get("/api/v1/report/posts")]
+pub fn get_posts_report(conn: DBConnection) -> ApiResponse {
+    //let tags = TagEntity::all(&*conn).unwrap();
+    ApiResponse::new(
+        Status::Ok,
+        json!({
+            "todo" : "TODO"
+        }),
+    )
+}
