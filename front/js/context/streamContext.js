8b97428cdf6f79aa97db9a744d272ed200ca41be
Aurélien <aurelien_lef@hotmail.fr>
2020-05-09 23:26:21 +0200

    Added poll vote

--- front/js/context/streamContext.js
+++ front/js/context/streamContext.js
@@ -74,9 +74,24 @@ export function StreamProvider({ children }) {
       },
       value: [],
       of(id) {
-        const prefetch = this.value.filter((p) => p.id === id);
-        if (prefetch.length) return Promise.resolve(prefetch[0]);
-        return api.posts.of(id);
+        const prefetch = this.value.filter((p) => p.id == id);
+        let promise;
+        if (prefetch.length) {
+          promise = Promise.resolve(prefetch[0]);
+        } else {
+          promise = api.posts.of(id);
+        }
+
+        return promise.then((post) => {
+          if (post.kind == 'poll') {
+            return api.posts.pollData(id).then((pollData) => {
+              post.answers = pollData.answers;
+              post.userAnswer = pollData.userAnswer;
+              return post;
+            });
+          }
+          return post;
+        });
       },
       add(post) {
         const promise = api.posts.add(post);
@@ -125,6 +140,12 @@ export function StreamProvider({ children }) {
       },
       watch(post) {
         return this._updatePost(api.posts.watch(post.id));
+      },
+      pollData(id) {
+        return api.posts.pollData(id);
+      },
+      pollVote(postId, answerId) {
+        return this._updatePost(api.posts.pollVote(postId, answerId));
       },
     },
 
