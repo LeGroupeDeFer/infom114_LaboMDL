708823250c49ac51d08c77a87d28d6d96704289a
sacha <raven.demeyer@gmail.com>
2020-05-10 23:42:55 +0200

    remove icon for login/logout from sidebar

--- front/js/lib/layout.js
+++ front/js/lib/layout.js
@@ -16,6 +16,7 @@ const nobodyLinks = [
 ];
 
 const authenticatedLinks = [];
+//  link for notifications but not implemented yet
 //  link('notifications', '/notifications', "bell", 'Notifications', 2)
 
 
